98c79cb424f3d0b3792acea0ed302b6f966fc940
dnk89 <dmitrij.kozlovskij@gmail.com>
2018-03-29 22:21:58 +0300

    Spring boot web initial content

---
+++ src/main/java/edu/dnk89/userfront/userfront/UserfrontApplication.java
@@ -0,0 +1,12 @@
+package edu.dnk89.userfront.userfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserfrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserfrontApplication.class, args);
+	}
+}
